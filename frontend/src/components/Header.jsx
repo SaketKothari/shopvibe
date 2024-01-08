fc841ee369d450febd16eab96ed87ec9261ee41e
Saket Kothari <saketkothari722@gmail.com>
2024-01-08 21:09:11 +0530

    implemented react-router-dom & react-router-bootstrap

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Navbar, Nav, Container, NavbarCollapse } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 
 import logo from '../assets/logo.png';
@@ -8,20 +9,29 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
-            <img src={logo} alt="ShopVibe" />
-            ShopVibe
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <NavbarCollapse id="basic-navbar-nav" />
-          <Nav className="ms-auto">
-            <Nav.Link href="/cart">
-              <FaShoppingCart /> Cart
-            </Nav.Link>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} alt="ShopVibe" />
+              ShopVibe
+            </Navbar.Brand>
+          </LinkContainer>
 
-            <Nav.Link href="/login">
-              <FaUser /> Sign In
-            </Nav.Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+          <NavbarCollapse id="basic-navbar-nav" />
+
+          <Nav className="ms-auto">
+            <LinkContainer to="/cart">
+              <Nav.Link>
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+            </LinkContainer>
+
+            <LinkContainer to="/login">
+              <Nav.Link>
+                <FaUser /> Sign In
+              </Nav.Link>
+            </LinkContainer>
           </Nav>
         </Container>
       </Navbar>
