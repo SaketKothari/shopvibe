368198e643714def55415fc8b6c3738dd4b5bc3e
Saket Kothari <saketkothari722@gmail.com>
2024-01-16 23:24:21 +0530

    custom error middleware

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,24 @@
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  // if its 200 is should be 500
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+
+  // Check for Mongoose Bad ObjectId
+  if (err.name === 'CastError' && err.kind === 'ObjectId') {
+    message = 'Resource not found';
+    statusCode = 404;
+  }
+
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+  });
+};
+
+export { notFound, errorHandler };
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 import connectDB from './config/db.js';
 import productRoutes from './routes/product.routes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
 
@@ -17,5 +18,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
