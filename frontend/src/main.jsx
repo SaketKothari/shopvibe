4241c0c434857d4fb9a8e3bd4431fe552d0ea3d2
Saket Kothari <saketkothari722@gmail.com>
2024-01-21 23:34:23 +0530

    cart screen layout & working

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,7 @@ import {
 import { Provider } from 'react-redux';
 
 import App from './App.jsx';
+import CartScreen from './screens/CartScreen.jsx';
 import HomeScreen from './screens/HomeScreen.jsx';
 import ProductScreen from './screens/ProductScreen.jsx';
 import store from './store.js';
@@ -22,6 +23,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
     </Route>
   )
 );
