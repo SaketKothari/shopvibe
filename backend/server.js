77177cfd213bd2bddec900851aa3522b0748ed0c
Saket Kothari <saketkothari722@gmail.com>
2024-01-22 23:53:09 +0530

    user routes & controllers

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 import connectDB from './config/db.js';
 import productRoutes from './routes/product.routes.js';
+import userRoutes from './routes/user.routes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
@@ -18,6 +19,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
