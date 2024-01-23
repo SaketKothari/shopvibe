f9dd07f8c447b798f69c49b20a99c9e4e1cc0504
Saket Kothari <saketkothari722@gmail.com>
2024-01-23 23:18:36 +0530

    jwt http-only cookie

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,5 @@
+import jwt from 'jsonwebtoken';
+
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
 
@@ -10,6 +12,18 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '20d',
+    });
+
+    // Set JWT as HTTP-only cookie
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== 'development',
+      sameSite: 'strict',
+      maxAge: 20 * 24 * 60 * 60 * 1000, // 20 days
+    });
+
     res.json({
       _id: user._id,
       name: user.name,
