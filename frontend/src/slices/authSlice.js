aa87743365d30a3c89a69864c9b92e647469f20e
Saket Kothari <saketkothari722@gmail.com>
2024-01-26 21:51:16 +0530

    authSlice & userApiSlice

---
+++ frontend/src/slices/authSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  userInfo: localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      // Once we hit the backend through user apiSlice we get our userInfo, sent it here in action in payload
+      state.userInfo = action.payload;
+      localStorage.setItem('userInfo', JSON.stringify(action.payload));
+    },
+  },
+});
+
+export const { setCredentials } = authSlice.actions;
+
+export default authSlice.reducer;
---
+++ frontend/src/slices/userApiSlice.js
@@ -0,0 +1,18 @@
+import { apiSlice } from './apiSlice';
+import { USERS_URL } from '../constants';
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    // Now we can dispatch this login action from our login screen
+    login: builder.mutation({
+      query: (data) => ({
+        url: USERS_URL / auth,
+        method: 'POST',
+        body: data,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+  }),
+});
+
+export const { useLoginMutation } = usersApiSlice;
